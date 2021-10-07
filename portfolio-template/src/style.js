ce2c0a795c9c93bb83f2750854ba33cbd7f5c462
Master-YEET <christopher.robidas@outlook.com>
2021-10-07 10:29:18 -0400

    Main body of portfolio done

---
+++ portfolio-template/src/style.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const AboutSection = styled.section`
+    background-color: #FFFFFF;
+    height: 100vh;
+`;
+
+export const ContactSection = styled.section`
+    background-color: #1D1D1D;
+    height: 100vh;
+`;
+
+export const Footer = styled.footer`
+    align-items: center;
+    background-color: #0D0D0D;
+    display: flex;
+    justify-content: center;
+    padding: 50px 0px;
+`;
+
+export const FooterLastUpdated = styled.p`
+    color: #91FE3C;
+    display: inline;
+`;
+
+export const FooterText = styled.p`
+    color: #999999;
+    flex: 0 0 auto;
+`;
+
+export const HomeSection = styled.section`
+    align-items: center;
+    background-color: #1D1D1D;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+`;
+
+export const HomeText = styled.p`
+    color: #FFFFFF;
+    flex: 0 0 auto;
+    font-size: 60px;
+    font-weight: bold;
+`;
+
+export const NameText = styled.p`
+    color: #91FE3C;
+    display: inline;
+`;
+
+export const ProjectsSection = styled.section`
+    background-color: #AAAAAA;
+    height: 100vh;
+`;
+
+export const SectionTitle = styled.h1`
+    color: #E31B6D;
+    font-size: 50px;
+    font-weight: bold;
+    margin: 0px;
+    padding: 10px;
+`;
